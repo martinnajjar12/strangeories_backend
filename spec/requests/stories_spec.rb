7ee9613b94e1699225b6150dcb62c2e5827b3762
Martin Najjar <martin_najjar@yahoo.com>
2021-07-09 20:16:53 +0300

    Add tests to the stories controller methods

--- spec/requests/stories_spec.rb
+++ spec/requests/stories_spec.rb
@@ -1,7 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe "Stories", type: :request do
+  before(:each) do
+    post '/api/v1/stories', params: { title: 'title', author: 'John', description: 'description', image_url: 'some url' }
+  end
+
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it 'should return 200 status' do
+      get '/api/v1/stories'
+
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe "POST /create" do
+    it 'should return a 201 status' do
+      post '/api/v1/stories', params: { author: 'Martin', title: 'test story', description: 'some description', image_url: 'some url'}
+
+      expect(response).to have_http_status(201)
+    end
   end
 end
