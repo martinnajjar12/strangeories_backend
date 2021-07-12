524808b25f59c48bcebb2de217e66fed1b3d7490
Martin Najjar <martin_najjar@yahoo.com>
2021-07-13 00:42:07 +0300

    Fix rubocop issues

--- app/models/dislike.rb
+++ app/models/dislike.rb
@@ -6,7 +6,6 @@ class Dislike < ApplicationRecord
 
   def count_scoped_to_story_and_user_ip_address
     errors.add(:dislike_id, 'You have already disliked this story!') if Story.current.dislikes
-                                                                             .where(user_ip == Dislike.current_ip
-                                                                             .to_json).exists?
+      .where(user_ip == Dislike.current_ip.to_json).exists?
   end
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -6,7 +6,6 @@ class Like < ApplicationRecord
 
   def count_scoped_to_story_and_user_ip_address
     errors.add(:like_id, 'You have already disliked this story!') if Story.current.likes
-                                                                          .where(user_ip == Like.current_ip
-                                                                          .to_json).exists?
+      .where(user_ip == Like.current_ip.to_json).exists?
   end
 end
