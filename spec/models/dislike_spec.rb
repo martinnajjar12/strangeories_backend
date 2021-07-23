bdb60e961ad9e40549ef975bd42a71e3e4dab168
Martin Najjar <martin_najjar@yahoo.com>
2021-07-23 22:45:36 +0300

    Final commit

--- spec/models/dislike_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Dislike, type: :model do
-  describe 'association' do
-    it { should belong_to(:story) }
-  end
-end
--- spec/models/like_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Like, type: :model do
-  describe 'association' do
-    it { should belong_to(:story) }
-  end
-end
