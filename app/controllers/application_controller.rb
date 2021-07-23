12b6a0a1a0d8244dea387febd543afa1e14f0399
Martin Najjar <martin_najjar@yahoo.com>
2021-07-23 22:39:32 +0300

    Fix set_current_story bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,8 @@
 class ApplicationController < ActionController::API
-  before_action :set_current_ip
-  before_action :set_current_story
+  before_action :set_current_ip, :set_current_story
 
   def set_current_story
-    if params.keys.include?(:story_id)
+    if params.keys.include?("story_id")
       @current_story = Story.find(params[:story_id])
       Story.current = @current_story
     end
