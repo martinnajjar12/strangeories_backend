ea3bdb4bb41be97e530d00670e901bc3ed87ee2c
Martin Najjar <martin_najjar@yahoo.com>
2021-07-23 22:24:50 +0300

    Sovle ApplicationController isuue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,16 @@
 class ApplicationController < ActionController::API
-  before_action :set_current_ip, :cors_preflight_check
-  before_action :set_current_story if Story.find(params[:story_id]).exists?
+  before_action :set_current_ip
+  before_action :set_current_story
 
   def set_current_story
-    @current_story = Story.find(params[:story_id])
-    Story.current = @current_story
+    if params.keys.include?(:story_id)
+      @current_story = Story.find(params[:story_id])
+      Story.current = @current_story
+    end
   end
 
   def set_current_ip
     Dislike.current_ip = request.remote_ip
     Like.current_ip = request.remote_ip
-  end
-
-  def cors_preflight_check
-    headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
-    headers['Access-Control-Request-Method'] = '*'
-    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   end
 end
