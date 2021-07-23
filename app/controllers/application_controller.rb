a8f83bf987bd56677c18f95dd3b0fc8d2d1cca08
Martin Najjar <martin_najjar@yahoo.com>
2021-07-23 16:40:26 +0300

    Create a cors_preflight_check method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::API
-  before_action :set_current_ip
+  before_action :set_current_ip, :cors_preflight_check
   before_action :set_current_story if Story.find(params[:story_id]).exists?
 
   def set_current_story
@@ -10,5 +10,12 @@ class ApplicationController < ActionController::API
   def set_current_ip
     Dislike.current_ip = request.remote_ip
     Like.current_ip = request.remote_ip
+  end
+
+  def cors_preflight_check
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   end
 end
