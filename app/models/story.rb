c4bf31ad4d8c1d982acd50af09e10ee9faddc972
Martin Najjar <martin_najjar@yahoo.com>
2021-08-19 23:54:25 +0300

    Validate presence of image

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,5 +1,5 @@
 class Story < ApplicationRecord
-  validates_presence_of :title, :description, :image_url
+  validates_presence_of :title, :description, :image
 
   has_one_attached :image
   has_many :likes
