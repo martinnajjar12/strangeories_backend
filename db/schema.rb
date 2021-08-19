f7bc406c32a1d50347c4c1ed6db9b5b2322773dc
Martin Najjar <martin_najjar@yahoo.com>
2021-08-19 23:08:10 +0300

    Remove count columns from likes and dislikes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_19_192018) do
+ActiveRecord::Schema.define(version: 2021_08_19_200710) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,7 +70,6 @@ ActiveRecord::Schema.define(version: 2021_08_19_192018) do
 
   create_table "dislikes", force: :cascade do |t|
     t.bigint "story_id", null: false
-    t.integer "count"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "author_id", null: false
@@ -80,7 +79,6 @@ ActiveRecord::Schema.define(version: 2021_08_19_192018) do
 
   create_table "likes", force: :cascade do |t|
     t.bigint "story_id", null: false
-    t.integer "count"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "author_id", null: false
